b3a9293a930a40e5550786968b30ca72ab3153ab
TiagoFreitas03 <tiago.freitas.faria@gmail.com>
2025-05-28 20:51:36 -0300

    create entity Task

---
+++ backend/src/domain/entities/task.ts
@@ -0,0 +1,56 @@
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { Optional } from '@/core/types/optional'
+
+enum TaskStatus {
+  TO_DO = 'TO_DO',
+  DOING = 'DOING',
+  DONE = 'DONE',
+}
+
+export interface TaskProps {
+  name: string
+  description: string
+  status: TaskStatus
+  priority: number
+  createdAt: Date
+  updatedAt?: Date
+}
+
+export class Task extends Entity<TaskProps> {
+  get name() {
+    return this.props.name
+  }
+
+  get description() {
+    return this.props.description
+  }
+
+  get status() {
+    return this.props.status
+  }
+
+  get priority() {
+    return this.props.priority
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  static create(props: Optional<TaskProps, 'createdAt'>, id?: UniqueEntityId) {
+    const task = new Task(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id,
+    )
+
+    return task
+  }
+}
