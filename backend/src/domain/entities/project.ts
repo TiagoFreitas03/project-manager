e2af1f17ed315a3f8018794c892e909991141acc
TiagoFreitas03 <tiago.freitas.faria@gmail.com>
2025-05-27 21:39:56 -0300

    create entity Project

---
+++ backend/src/domain/entities/project.ts
@@ -0,0 +1,50 @@
+import { Entity } from '@/core/entities/entity'
+import { Slug } from './value-objects/slug'
+import { Optional } from '@/core/types/optional'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+
+export interface ProjectProps {
+  name: string
+  description: string
+  slug: Slug
+  createdAt: Date
+  updatedAt?: Date
+}
+
+export class Project extends Entity<ProjectProps> {
+  get name() {
+    return this.props.name
+  }
+
+  get description() {
+    return this.props.description
+  }
+
+  get slug() {
+    return this.props.slug
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  static create(
+    props: Optional<ProjectProps, 'createdAt' | 'slug'>,
+    id?: UniqueEntityId,
+  ) {
+    const project = new Project(
+      {
+        ...props,
+        slug: props.slug ?? Slug.createFromText(props.name),
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id,
+    )
+
+    return project
+  }
+}
