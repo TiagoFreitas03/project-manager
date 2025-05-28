ff05bde3d32b6f2193182defd5cfc0297724e115
TiagoFreitas03 <tiago.freitas.faria@gmail.com>
2025-05-27 22:14:07 -0300

    add methods findById and save

--- backend/src/domain/repositories/projects-repository.ts
+++ backend/src/domain/repositories/projects-repository.ts
@@ -2,4 +2,6 @@ import { Project } from '../entities/project'
 
 export interface ProjectsRepository {
   create(project: Project): Promise<void>
+  findById(id: string): Promise<Project | null>
+  save(project: Project): Promise<void>
 }
--- backend/test/repositories/in-memory-projects-repository.ts
+++ backend/test/repositories/in-memory-projects-repository.ts
@@ -7,4 +7,20 @@ export class InMemoryProjectsRepository implements ProjectsRepository {
   async create(project: Project) {
     this.items.push(project)
   }
+
+  async findById(id: string) {
+    const project = this.items.find((item) => item.id.toString() === id)
+
+    if (!project) {
+      return null
+    }
+
+    return project
+  }
+
+  async save(project: Project) {
+    const itemIndex = this.items.findIndex((item) => item.id === project.id)
+
+    this.items[itemIndex] = project
+  }
 }
