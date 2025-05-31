75a51430505214ad10e4f54debf8635852211197
TiagoFreitas03 <tiago.freitas.faria@gmail.com>
2025-05-30 22:42:59 -0300

    create projects repository for prisma

---
+++ backend/src/infra/repositories/prisma-projects-repository.ts
@@ -0,0 +1,72 @@
+import { Project } from '@/domain/entities/project'
+import {
+  ProjectsRepository,
+  SearchProjectsFilters,
+} from '@/domain/repositories/projects-repository'
+import { PrismaProjectMapper } from '../mappers/project-mapper'
+import { prisma } from '../lib/prisma'
+
+export class PrismaProjectsRepository implements ProjectsRepository {
+  async create(project: Project) {
+    const data = PrismaProjectMapper.toPrisma(project)
+
+    await prisma.project.create({ data })
+  }
+
+  async findById(id: string) {
+    const project = await prisma.project.findUnique({
+      where: { id },
+    })
+
+    if (!project) {
+      return null
+    }
+
+    return PrismaProjectMapper.toDomain(project)
+  }
+
+  async save(project: Project) {
+    const data = PrismaProjectMapper.toPrisma(project)
+
+    await prisma.project.update({
+      data,
+      where: {
+        id: data.id,
+      },
+    })
+  }
+
+  async searchMany({ name = '', orderBy, order, page }: SearchProjectsFilters) {
+    const projects = await prisma.project.findMany({
+      where: {
+        name: { contains: name },
+      },
+      orderBy: {
+        [orderBy]: [order],
+      },
+      skip: page * 20 - 20,
+    })
+
+    return projects.map(PrismaProjectMapper.toDomain)
+  }
+
+  async findBySlug(slug: string) {
+    const project = await prisma.project.findUnique({
+      where: { slug },
+    })
+
+    if (!project) {
+      return null
+    }
+
+    return PrismaProjectMapper.toDomain(project)
+  }
+
+  async delete(project: Project) {
+    await prisma.project.delete({
+      where: {
+        id: project.id.toString(),
+      },
+    })
+  }
+}
