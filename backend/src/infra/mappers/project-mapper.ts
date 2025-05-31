f5e99accc671b7d9aec788a18679c1222cc11802
TiagoFreitas03 <tiago.freitas.faria@gmail.com>
2025-05-30 22:42:01 -0300

    create mapper for entity Project

---
+++ backend/src/infra/mappers/project-mapper.ts
@@ -0,0 +1,32 @@
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { Project } from '@/domain/entities/project'
+import { Slug } from '@/domain/entities/value-objects/slug'
+import { Prisma, Project as PrismaProject } from 'generated/prisma'
+
+export class PrismaProjectMapper {
+  static toDomain(raw: PrismaProject): Project {
+    return Project.create(
+      {
+        name: raw.name,
+        description: raw.description,
+        repositoryUrl: raw.repositoryUrl,
+        slug: Slug.create(raw.slug),
+        createdAt: raw.createdAt,
+        updatedAt: raw.updatedAt,
+      },
+      new UniqueEntityId(raw.id),
+    )
+  }
+
+  static toPrisma(project: Project): Prisma.ProjectUncheckedCreateInput {
+    return {
+      id: project.id.toString(),
+      name: project.name,
+      description: project.description,
+      repositoryUrl: project.repositoryUrl,
+      slug: project.slug.value,
+      createdAt: project.createdAt,
+      updatedAt: project.updatedAt,
+    }
+  }
+}
