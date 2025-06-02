e0c5a15dfecfc4b62e02f4418002c907619ccc61
TiagoFreitas03 <tiago.freitas.faria@gmail.com>
2025-06-01 21:41:54 -0300

    add sorting to findManyByProjectId method

--- backend/src/infra/repositories/prisma-tasks-repository.ts
+++ backend/src/infra/repositories/prisma-tasks-repository.ts
@@ -13,6 +13,7 @@ export class PrismaTasksRepository implements TasksRepository {
   async findManyByProjectId(projectId: string) {
     const tasks = await prisma.task.findMany({
       where: { projectId },
+      orderBy: [{ priority: 'asc' }, { createdAt: 'desc' }],
     })
 
     return tasks.map(TaskMapper.toDomain)
