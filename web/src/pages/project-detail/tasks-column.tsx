4e10ca0ba74bbddece86463a4a67a27fda1e57a1
TiagoFreitas03 <tiago.freitas.faria@gmail.com>
2025-06-17 20:20:23 -0300

    add fields to task

--- web/src/pages/project-detail/tasks-column.tsx
+++ web/src/pages/project-detail/tasks-column.tsx
@@ -1,10 +1,14 @@
 import { StatusBadge } from '@/components/status-badge'
 import { ScrollArea } from '@/components/ui/scroll-area'
+import { format } from 'date-fns'
+import { MoveRight } from 'lucide-react'
 
 interface Task {
   name: string
   status: string
   priority: number
+  createdAt: Date
+  updatedAt: Date
 }
 
 interface TasksColumnProps {
@@ -28,8 +32,14 @@ export function TasksColumn({ status, tasks }: TasksColumnProps) {
           return (
             <div key={task.name} className="bg-neutral-800 my-3 p-4 rounded">
               <h2>{task.name}</h2>
+
               <span className="text-sm my-4">
                 Prioridade: {priorityNames[task.priority - 1]}
+              </span>
+
+              <span className="flex gap-2 text-xs mt-2">
+                {format(task.createdAt, 'dd MMM yyyy')} <MoveRight size={14} />{' '}
+                {format(task.updatedAt, 'dd MMM yyyy')}
               </span>
             </div>
           )
