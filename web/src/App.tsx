b3400b63025b9a2d850737bee4e3ad55cf8c54d9
TiagoFreitas03 <tiago.freitas.faria@gmail.com>
2025-06-16 22:43:38 -0300

    add base structure to pages

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,11 @@
 import { Router } from './router'
 
 export function App() {
-  return <Router />
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <div className="max-w-7xl w-full max-h-[48rem] h-full border-2 rounded m-4 bg-neutral-900 p-4">
+        <Router />
+      </div>
+    </div>
+  )
 }
--- web/src/pages/home/index.tsx
+++ web/src/pages/home/index.tsx
@@ -7,6 +7,7 @@ import { Header } from './header'
 import { FiltersForm } from './filters-form'
 import { Pagination } from '@/components/pagination'
 import { useState } from 'react'
+import { Link } from 'react-router'
 
 setDefaultOptions({ locale: ptBR })
 
@@ -20,6 +21,7 @@ const projects = Array.from({ length: 12 }).map((_, index) => {
     createdAt: new Date(2023, index, 1),
     updatedAt: new Date(2024, index, 1),
     status,
+    slug: `project-${index + 1}`,
     progress,
   }
 })
@@ -28,38 +30,40 @@ export function Home() {
   const [page, setPage] = useState(1)
 
   return (
-    <div className="w-screen h-screen flex items-center justify-center">
-      <div className="max-w-7xl w-full max-h-[48rem] h-full border-2 rounded m-4 bg-neutral-900 p-4">
-        <Header />
+    <>
+      <Header />
 
-        <FiltersForm />
+      <FiltersForm />
 
-        <div className="my-4 grid grid-cols-3 gap-3">
-          {projects.map((project) => {
-            return (
-              <div key={project.id} className="bg-neutral-800 p-3 rounded">
-                <h2 className="font-semibold mb-2">{project.name}</h2>
+      <div className="my-4 grid grid-cols-3 gap-3">
+        {projects.map((project) => {
+          return (
+            <Link
+              key={project.id}
+              className="bg-neutral-800 p-3 rounded"
+              to={`/project/${project.slug}`}
+            >
+              <h2 className="font-semibold mb-2">{project.name}</h2>
 
-                <span className="flex text-xs gap-2 items-center mb-2">
-                  {format(project.createdAt, 'dd MMM yyyy')}{' '}
-                  <MoveRight size={14} />{' '}
-                  {format(project.updatedAt, 'dd MMM yyyy')}
-                </span>
+              <span className="flex text-xs gap-2 items-center mb-2">
+                {format(project.createdAt, 'dd MMM yyyy')}{' '}
+                <MoveRight size={14} />{' '}
+                {format(project.updatedAt, 'dd MMM yyyy')}
+              </span>
 
-                <StatusBadge status={project.status} />
+              <StatusBadge status={project.status} />
 
-                <span className="flex gap-1 items-center text-xs mt-2 w-36">
-                  {project.progress}% <Progress value={project.progress} />
-                </span>
-              </div>
-            )
-          })}
-        </div>
-
-        <div className="flex justify-between items-center">
-          <Pagination pages={3} currentPage={page} onPageChange={setPage} />
-        </div>
+              <span className="flex gap-1 items-center text-xs mt-2 w-36">
+                {project.progress}% <Progress value={project.progress} />
+              </span>
+            </Link>
+          )
+        })}
       </div>
-    </div>
+
+      <div className="flex justify-between items-center">
+        <Pagination pages={3} currentPage={page} onPageChange={setPage} />
+      </div>
+    </>
   )
 }
