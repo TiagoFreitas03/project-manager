a9f3e6cb5934b523d81ffcd5e4c13aeae2d6cb36
TiagoFreitas03 <tiago.freitas.faria@gmail.com>
2025-06-19 18:43:15 -0300

    alter status badge component

--- web/src/components/status-badge.tsx
+++ web/src/components/status-badge.tsx
@@ -1,17 +1,18 @@
+import type { Status } from '@/types/status'
 import clsx from 'clsx'
 
 interface StatusBadgeProps {
-  status: string
+  value: Status
   size?: 'xs' | 'sm'
 }
 
-export function StatusBadge({ status, size = 'xs' }: StatusBadgeProps) {
+export function StatusBadge({ value, size = 'xs' }: StatusBadgeProps) {
   function formatStatus() {
-    if (status === 'todo') {
+    if (value === 'TO_DO') {
       return 'Pendente'
     }
 
-    if (status === 'doing') {
+    if (value === 'DOING') {
       return 'Em andamento'
     }
 
@@ -21,9 +22,9 @@ export function StatusBadge({ status, size = 'xs' }: StatusBadgeProps) {
   return (
     <span
       className={clsx('text-neutral-100 rounded-lg px-2 py-1', {
-        'bg-zinc-700/50': status === 'todo',
-        'bg-orange-600/50': status === 'doing',
-        'bg-pink-600/50': status === 'done',
+        'bg-zinc-700/50': value === 'TO_DO',
+        'bg-orange-600/50': value === 'DOING',
+        'bg-pink-600/50': value === 'DONE',
         'text-xs': size === 'xs',
         'text-sm': size === 'sm',
       })}
