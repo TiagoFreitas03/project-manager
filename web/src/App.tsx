86e9a4441b1d5684d9254247b7e356a012543339
TiagoFreitas03 <tiago.freitas.faria@gmail.com>
2025-06-22 17:54:19 -0300

    add sonner Toaster in App

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,17 @@
 import './lib/date-fns'
+import { Toaster } from './components/ui/sonner'
 import { Router } from './router'
 
 export function App() {
   return (
-    <div className="w-screen h-screen flex items-center justify-center">
-      <div className="max-w-7xl w-full max-h-[48rem] h-full border-2 rounded m-4 bg-neutral-900 p-4">
-        <Router />
+    <>
+      <div className="w-screen h-screen flex items-center justify-center">
+        <div className="max-w-7xl w-full max-h-[48rem] h-full border-2 rounded m-4 bg-neutral-900 p-4">
+          <Router />
+        </div>
       </div>
-    </div>
+
+      <Toaster />
+    </>
   )
 }
