4e606bb926cd4d4194306e2732e345c82e2ca293
TiagoFreitas03 <tiago.freitas.faria@gmail.com>
2025-06-22 15:25:07 -0300

    create value object project summary

---
+++ backend/src/domain/entities/value-objects/project-summary.ts
@@ -0,0 +1,42 @@
+import { ValueObject } from '@/core/entities/value-object'
+import { Slug } from './slug'
+import { Status } from './status'
+
+export interface ProjectSummaryProps {
+  slug: Slug
+  name: string
+  createdAt: Date
+  updatedAt: Date
+  status: Status
+  progress: number
+}
+
+export class ProjectSummary extends ValueObject<ProjectSummaryProps> {
+  get slug() {
+    return this.props.slug
+  }
+
+  get name() {
+    return this.props.name
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  get status() {
+    return this.props.status
+  }
+
+  get progress() {
+    return this.props.progress
+  }
+
+  static create(props: ProjectSummaryProps) {
+    return new ProjectSummary(props)
+  }
+}
