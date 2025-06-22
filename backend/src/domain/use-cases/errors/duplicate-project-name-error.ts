7250213ecc3a4dfa14344551ebd10cb13dcc12f6
TiagoFreitas03 <tiago.freitas.faria@gmail.com>
2025-06-22 17:45:47 -0300

    change error message

--- backend/src/domain/use-cases/errors/duplicate-project-name-error.ts
+++ backend/src/domain/use-cases/errors/duplicate-project-name-error.ts
@@ -3,6 +3,6 @@ import { BadRequestError } from '@/core/errors/bad-request-error'
 export class DuplicateProjectNameError extends BadRequestError {
   constructor(projectName: string) {
     super()
-    this.message = `"A project named ${projectName} already exists`
+    this.message = `"A project named "${projectName}" already exists`
   }
 }
