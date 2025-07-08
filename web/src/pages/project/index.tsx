429fe5a7bfa8a7268c78126ab00373a4fbc2eced
TiagoFreitas03 <tiago.freitas.faria@gmail.com>
2025-07-07 21:24:52 -0300

    alter project page to fetch tasks from api

--- web/src/pages/project/index.tsx
+++ web/src/pages/project/index.tsx
@@ -10,42 +10,31 @@ import { Header } from '@/components/header'
 import { useParams } from 'react-router'
 import { useEffect, useState } from 'react'
 import { getProjectBySlug } from '@/api/get-project-by-slug'
+import { fetchProjectTasks } from '@/api/fetch-project-tasks'
 
-const tasks: Task[] = Array.from({ length: 21 })
-  .map((_, index) => {
-    return {
-      id: index.toString(),
-      name: `Task ${index + 1}`,
-      status: index < 7 ? 'TO_DO' : index < 14 ? 'DOING' : 'DONE',
-      priority: (index % 3) + 1,
-      createdAt: new Date(2025, index % 12, 1),
-      updatedAt: new Date(2025, index % 12, (index % 30) + 1),
-    }
-  })
-  .sort((a, b) => {
-    if (a.priority > b.priority) {
-      return 1
-    }
-
-    if (a.priority < b.priority) {
-      return -1
-    }
-
-    return 0
-  })
-
-const todoTasks = tasks.filter((task) => task.status === 'TO_DO')
-const doingTasks = tasks.filter((task) => task.status === 'DOING')
-const doneTasks = tasks.filter((task) => task.status === 'DONE')
+interface ProjectTasks {
+  toDoTasks: Task[]
+  doingTasks: Task[]
+  doneTasks: Task[]
+}
 
 export function Project() {
   const { slug } = useParams<{ slug: string }>()
 
   const [project, setProject] = useState<ProjectType>()
+  const [tasks, setTasks] = useState<ProjectTasks>({
+    toDoTasks: [],
+    doingTasks: [],
+    doneTasks: [],
+  })
 
   useEffect(() => {
     if (slug) {
-      getProjectBySlug(slug).then((data) => setProject(data))
+      getProjectBySlug(slug).then((data) => {
+        setProject(data)
+
+        fetchProjectTasks(data.id).then((data) => setTasks(data))
+      })
     }
   }, [slug])
 
@@ -72,11 +61,11 @@ export function Project() {
       </Header>
 
       <div className="grid grid-cols-3 mt-2 gap-2">
-        <TasksColumn status={'TO_DO'} tasks={todoTasks} />
+        <TasksColumn status={'TO_DO'} tasks={tasks.toDoTasks} />
 
-        <TasksColumn status={'DOING'} tasks={doingTasks} />
+        <TasksColumn status={'DOING'} tasks={tasks.doingTasks} />
 
-        <TasksColumn status={'DONE'} tasks={doneTasks} />
+        <TasksColumn status={'DONE'} tasks={tasks.doneTasks} />
       </div>
     </>
   )
