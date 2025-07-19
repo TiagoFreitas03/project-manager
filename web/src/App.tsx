1c9989e27d40ff9d3482da5aa8517da901ea0129
TiagoFreitas03 <tiago.freitas.faria@gmail.com>
2025-07-18 21:34:02 -0300

    install and config react query

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,14 @@
-import './lib/date-fns'
+import { QueryClientProvider } from '@tanstack/react-query'
 import { Toaster } from './components/ui/sonner'
 import { Router } from './router'
+import './lib/date-fns'
+import { queryClient } from './lib/react-query'
 
 export function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Router />
       <Toaster />
-    </>
+    </QueryClientProvider>
   )
 }
